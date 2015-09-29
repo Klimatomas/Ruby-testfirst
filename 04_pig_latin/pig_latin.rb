6bda7361eed8de01e248acfa878c75177e1cd23a
Tomáš Klíma <mrklimatomas@gmail.com>
2015-09-29 16:29:15 +0200

    final solution
    final solution to the pig_latin challenge

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,42 +1,24 @@
 def translate(x)
-    ary = x.split(" ")
+    ary = []
     vowels = %w(a e i o u y)
-    double = %w(qu)
-    ary.each do |word|
-        firstTwo = word.chars.first(2) 
-        firstLetter = word.chars.first
-        until vowels.include?(firstLetter)
-         if not double.include?(firstTwo)
-                z = word.slice!(0..1)
-                word << z
-                firstLetter = word.chars.first
-            else
-                 y = word.slice!(0)
-                word << y
-                 firstLetter = word.chars.first
-            end
-        end
-        word << "ay"
+    dual = %w(qu)
+    moreWords = x.split(" ")
+    moreWords.each do |x|
+    firstLetter = x.chars.first
+    firstTwo = x.chars.first(2).join
+    until  dual.include?(firstTwo) || vowels.include?(firstLetter)
+                x.slice!(0)
+                x << firstLetter
+                firstLetter = x.chars.first
+                firstTwo = x.chars.first(2).join
+    end
+    if dual.include?(firstTwo)
+            x.slice!(0..1)
+            x << firstTwo
+    end
+             x << "ay"
+            puts x
+            ary.push(x)
     end
     ary.join(" ")
-
 end
-
-
-=begin
-def translate(word)
-    vowels = %w(a e i o u y)
-    moreWords = word.split(" ")
-    moreWords.each do |x|
-       firstLetter = x.split("").first
-       if not vowels.include?(firstLetter)
-            x.slice!(0)            
-            x << firstLetter
-            translate(x)
-        else
-            x << "ay"            
-        	return x
-        end        
-    end     
-end
-=end
